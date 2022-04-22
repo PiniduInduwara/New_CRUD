3033364b9638bc4f5ffaa5673fc19e102a5c6185
PiniduInduwara <it20202286@my.sliit.lk>
2022-04-22 12:05:35 +0530

    Initial commit

---
+++ src/test/java/com/NewCrud/project/NewCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NewCrud.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
