e65c8c564c69ded92bbbc4554fd7060bbd7e5184
PiniduInduwara <it20202286@my.sliit.lk>
2022-04-26 16:28:20 +0530

    commit

--- src/main/java/com/NewCrud/project/repository/BooksRepository.java
+++ src/main/java/com/NewCrud/project/repository/BooksRepository.java
@@ -2,10 +2,12 @@ package com.NewCrud.project.repository;
 
 import com.NewCrud.project.model.Books;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BooksRepository extends MongoRepository<Books, Integer>{
-
 
 }
